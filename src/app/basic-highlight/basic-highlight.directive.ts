1964f64714dc31f598b3bcb2d24e3d0f22d68285
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-28 10:56:17 -0800

    changing color to white for readablity

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -9,5 +9,6 @@ export class BasicHighlightDirective implements OnInit {
 
   ngOnInit() {
     this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.elementRef.nativeElement.style.color = 'white';
   }
 }
